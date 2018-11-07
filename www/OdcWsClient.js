510c49ab487dd3d2f7ca9114a9de42ced2234133
João <joao.goncalves@xpand-it.com>
2018-11-07 00:21:48 +0000

    Renaming Semaphore to TrafficLight

--- www/OdcWsClient.js
+++ www/OdcWsClient.js
@@ -1,25 +1,25 @@
 var exec = require('cordova/exec');
 
-function Semaphore() {};
+function TrafficLight() {};
 
-Semaphore.prototype.onColorChanged = function (color) {
+TrafficLight.prototype.onColorChanged = function (color) {
     console.log("[Debug] onColorChanged:", color);
 };
 
-Semaphore.prototype.onConnectionClosed = function (result) {
+TrafficLight.prototype.onConnectionClosed = function (result) {
     console.log("[Debug] onConnectionClosed:", result);
 };
 
-var semaphore;
-exports.setSemaphore = function (newSemaphore) {
-    semaphore = newSemaphore;
+var trafficLight;
+exports.setTrafficLight = function (newTrafficLight) {
+    trafficLight = newTrafficLight;
 };
 
-module.exports.Semaphore = Semaphore;
+module.exports.TrafficLight = TrafficLight;
 
 module.exports.openConnection = function (ip, port, success, error) {
     console.log("[Debug] openConnection", ip, port);
-    if (semaphore === undefined) {
+    if (trafficLight === undefined) {
         console.error("[Debug] Semaphore not set! Please call setSemaphore first.");
         error({"type": "onError", "message": "Semaphore not set! Please call setSemaphore first."});
         return;
@@ -32,11 +32,11 @@ module.exports.openConnection = function (ip, port, success, error) {
         }
     
         if (result.type === "onColorChanged") {
-            semaphore.onColorChanged(result.color);
+            trafficLight.onColorChanged(result.color);
         } else if (result.type === "onOpen") {
             success(result);
         } else if(result.type === "onClose") {
-            semaphore.onConnectionClosed(result);
+            trafficLight.onConnectionClosed(result);
         } else {
             error(result);
         }
